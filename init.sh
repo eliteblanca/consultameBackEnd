b42139adec66113fc326e022a0c4bc149ef1e953
julian vargas <julian.vargas@grupokonecta.com>
2020-02-18 16:49:15 -0500

    se agrega archivo .sh

---
+++ init.sh
@@ -0,0 +1,20 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+nvm install node
+nvm use node
+sudo yum upgrade
+sudo yum install git
+git clone https://github.com/eliteblanca/consultameFrontEnd.git
+git clone https://github.com/eliteblanca/consultameBackEnd.git
+cd consultameBackEnd/
+npm install
+npm run build
+cd ../consultameFrontEnd
+npm install
+npm run buildprod
+sudo yum install nginx
+sudo yum install vim    
+sudo mv ngxapp.conf /etc/nginx/conf.d/
+sudo restorecon  /etc/nginx/conf.d/
+sudo restorecon  /etc/nginx/conf.d/*
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/server.key -out /etc/nginx/server.crt
+npm install pm2 -g
