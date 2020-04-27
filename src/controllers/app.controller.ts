8bc13e0a0d3fc11f541817d6351d1c9de37ac707
julian vargas <julian.vargas@grupokonecta.com>
2020-04-27 11:58:16 -0500

    se implementa reCaptcha

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { LdapService } from "../services/auth.service";
-
+import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
+import * as https from "https";
+import * as qs from "querystring"
 class user {
     "sub": string
     "name": string
@@ -25,5 +27,32 @@ export class AppController {
     @Get('me')
     currentUser(@Request() req): Promise<user> {
         return req.user
+    }
+
+    @UseGuards()
+    @Post('validateCaptcha')
+    async validateCaptcha(
+        @Body() body:{ token:string }
+    ) {
+
+        var RECAPTCHA_KEY = process.env.RECAPTCHA_KEY        
+        
+
+        const axiosInstance = axios.create({
+            httpsAgent: new https.Agent({  
+              rejectUnauthorized: false
+            })
+          });
+
+
+        let googleResponse = await axiosInstance.post('https://www.google.com/recaptcha/api/siteverify', 
+            qs.stringify({
+                secret:RECAPTCHA_KEY,
+                response:body.token
+            })
+        )
+
+        return googleResponse.data
+
     }
 }
