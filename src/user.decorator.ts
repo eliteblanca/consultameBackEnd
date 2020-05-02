eb2bc5eed2e87664d59b919c55396e9a5c023cf1
julian vargas <julian.vargas@grupokonecta.com>
2020-05-02 12:10:35 -0500

    hot fix user decorator

--- src/user.decorator.ts
+++ src/user.decorator.ts
@@ -2,9 +2,12 @@ import { createParamDecorator } from '@nestjs/common';
 import { User as U } from "./entities/user";
 
 export const User = createParamDecorator((data, req):U => {
-  if(req.args[0]?.user){
-    return req.args[0].user;
+  if(req.args){
+    if(req.args[0]?.user){
+      return req.args[0].user;
+    }
   }
+  
   if(req.user){
     return req.user
   }
